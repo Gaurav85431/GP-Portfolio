714b23f957c3183233f9dbe0bc139e7020000772
Gaurav Pushpam <145956263+Gaurav85431@users.noreply.github.com>
2024-07-13 00:50:25 +0530

    first

---
+++ src/components/Contact.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { FaInstagram } from "react-icons/fa";
+import { CiFacebook } from "react-icons/ci";
+import { CiLinkedin } from "react-icons/ci";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaGithubSquare } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+
+const Contact = () => {
+  return (
+    <>
+      <div className="container contact" id="contact">
+        <h1>CONTACT ME</h1>
+        <h3>📧 pushpamgaurav@gmail.com</h3>
+        <h3>📱 9263560601</h3>
+        <div
+          className="contact-icon"
+          data-aos="zoom-in-up"
+          data-aos-duration="1000"
+        >
+          <a href="https://www.instagram.com" target="_blank" className="items">
+            <FaInstagram className="icons" />
+          </a>
+          <a href="https://www.facebook.com" target="_blank" className="items">
+            <CiFacebook className="icons" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/gaurav-pushpam-a049062a1/"
+            target="_blank"
+            className="items"
+          >
+            <CiLinkedin className="icons" />
+          </a>
+          <a href="https://www.twitter.com" target="_blank" className="items">
+            <FaSquareXTwitter className="icons" />
+          </a>
+          <a href="https://www.github.com" target="_blank" className="items">
+            <FaGithubSquare className="icons" />
+          </a>
+          <a
+            href="mailto:pushpamgaurav@gmail.com"
+            target="_blank"
+            className="items"
+          >
+            <SiGmail className="icons" />
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
